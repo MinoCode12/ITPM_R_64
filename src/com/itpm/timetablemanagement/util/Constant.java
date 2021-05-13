a78504545a3533ddaed0a94dd134f91caa7c4eb9
“GayanKavinda” <gayan982822513.lk@gmail.com>
2021-05-14 00:22:20 +0530

    first commit

---
+++ src/com/itpm/timetablemanagement/util/Constant.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itpm.timetablemanagement.util;
+
+/**
+ *
+ * @author gayan
+ */
+public class Constant {
+    
+    public static final String CREATE_DATABASE = "create_database";
+    
+   
+    public static final String INSERT_WORKING_HOUR_TABLE = "Insert_workingdays_and_hours";
+    public static final String INSERT_WE_WORKING_HOUR_DAYS_TABLE = "Insert_WE_workingdays_and_hours_days";
+    public static final String INSERT_WD_WORKING_HOUR_DAYS_TABLE = "Insert_WD_workingdays_and_hours_days";
+    public static final String REMOVE_WORKING_HOUR_TABLE = "Remove_workingdays_and_hours";
+    public static final String REMOVE_WORKING_HOUR_DAYS_TABLE = "Remove_workingdays_and_hours_days";
+    public static final String GET_WORKING_HOUR_TABLE = "Get_workingdays_and_hours";
+    public static final String GET_WORKING_HOUR_TABLE_BY_TYPE = "Get_workingdays_and_hours_by_type";
+    
+    public static final String QUERY = "query";
+    public static final String QUERY_ID = "id";
+    public static final String PROPERTIES = "timetablemanagement.properties";
+    public static final String DB_PASSWORD = "password";
+    public static final String DB_USERNAME = "username";
+    public static final String DB_URL = "url";
+    public static final String DB_DRIVER = "driver";
+    
+    
+}
