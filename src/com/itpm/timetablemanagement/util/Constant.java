ba1e7761bff6ea565028a3efd3f14faa0987314c
Hasini <hasinidilanka2266@gmail.com>
2021-05-21 18:55:45 +0530

    Member 2

--- src/com/itpm/timetablemanagement/util/Constant.java
+++ src/com/itpm/timetablemanagement/util/Constant.java
@@ -22,6 +22,21 @@ public class Constant {
     public static final String GET_WORKING_HOUR_TABLE = "Get_workingdays_and_hours";
     public static final String GET_WORKING_HOUR_TABLE_BY_TYPE = "Get_workingdays_and_hours_by_type";
     
+    public static final String INSERT_LECTURER = "insert_lecturer";
+    public static final String GET_LECTURER = "get_lecturer";
+    public static final String UPDATE_LECTURER = "update_lecturer";
+    public static final String DELETE_LECTURER= "delete_lecturer";
+    
+    public static final String INSERT_SUBJECTS = "insert_subject";
+    public static final String UPDATE_SUBJECT = "update_subject";
+    public static final String DELETE_SUBJECTS= "delete_subject";
+    public static final String GET_SUBJECTS = "get_subjects";
+    
+    public static final String INSERT_SESSION = "insert_session";
+    public static final String GET_SESSION = "get_session";
+    public static final String DELETE_SESSION= "delete_session";
+    public static final String UPDATE_SESSION = "update_session";
+    
     public static final String QUERY = "query";
     public static final String QUERY_ID = "id";
     public static final String PROPERTIES = "timetablemanagement.properties";
